1647c34fd4aeca7d5c3672b7d52c0f274cbd65fd
SirFrank0 <frankseb04@gmail.com>
2022-12-09 20:16:09 -0500

    var setted

---
+++ js/code.js
@@ -0,0 +1,20 @@
+function init(){
+    var resultado = document.getElementById('resultado');
+    var reset = document.getElementById('reset');
+    var suma = document.getElementById('suma');
+    var resta = document.getElementById('resta');
+    var mult = document.getElementById('mult');
+    var division = document.getElementById('division');
+    var igual = document.getElementById('igual');
+    var uno = document.getElementById('uno');
+    var dos = document.getElementById('dos');
+    var tres = document.getElementById('tres');
+    var cuatro = document.getElementById('cuatro');
+    var cinco = document.getElementById('cinco');
+    var seis = document.getElementById('seis');
+    var siete = document.getElementById('siete');
+    var ocho = document.getElementById('ocho');
+    var nueve = document.getElementById('nueve');
+    var cero = document.getElementById('cero');
+  }
+}
