4c0a98ea9b54b989f3a451d6a1af18402d91e59e
SirFrank0 <frankseb04@gmail.com>
2022-12-09 20:23:48 -0500

    events added

--- js/code.js
+++ js/code.js
@@ -1,4 +1,6 @@
 function init(){
+
+    //variables
     var resultado = document.getElementById('resultado');
     var reset = document.getElementById('reset');
     var suma = document.getElementById('suma');
@@ -16,5 +18,36 @@ function init(){
     var ocho = document.getElementById('ocho');
     var nueve = document.getElementById('nueve');
     var cero = document.getElementById('cero');
+
+    //eventos
+    uno.onclick = function(e){
+        resultado.textContent = resultado.textContent  + "1";
+    }
+    dos.onclick = function(e){
+        resultado.textContent = resultado.textContent  + "2";
+    }
+    tres.onclick = function(e){
+        resultado.textContent = resultado.textContent  + "3";
+    }
+    cuatro.onclick = function(e){
+        resultado.textContent = resultado.textContent  + "4";
+    }
+    cinco.onclick = function(e){
+        resultado.textContent = resultado.textContent  + "5";
+    }
+    seis.onclick = function(e){
+        resultado.textContent = resultado.textContent  + "6";
+    }
+    siete.onclick = function(e){
+        resultado.textContent = resultado.textContent  + "7";
+    }
+    ocho.onclick = function(e){
+        resultado.textContent = resultado.textContent  + "8";
+    }
+    nueve.onclick = function(e){
+        resultado.textContent = resultado.textContent  + "9";
+    }
+    cero.onclick = function(e){
+        resultado.textContent = resultado.textContent  + "0";
+    }
   }
-}
